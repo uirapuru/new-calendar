6d7ddb00a4e8984f010da0b8a6710acf7abd8785
Grzegorz 'Uirapuru' Kaszuba <uirapuruadg@gmail.com>
2018-06-30 01:19:08 +0200

    up

--- src/Calendar/Calendar.php
+++ src/Calendar/Calendar.php
@@ -2,23 +2,58 @@
 
 namespace Calendar;
 
+use DateInterval;
+use DatePeriod;
 use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Ramsey\Uuid\UuidInterface;
 
 class Calendar
 {
+    /** @var UuidInterface */
+    protected $id;
+
     /** @var Event[]|Collection|array */
     protected $events;
 
-    public function __construct(Collection $events)
+    public function __construct(UuidInterface $id, ?Collection $events = null)
     {
-        $this->events = $events;
+        $this->id = $id;
+        $this->events = $events ?? new ArrayCollection();
     }
 
-    public function filterEvents(DateTime $date1, ?DateTime $date2 = null) : Collection
+    public function id(): UuidInterface
     {
-        return $this->events->filter(function(Event $event) use ($date1, $date2) : bool {
-            return $event->isMatching($date1);
+        return $this->id;
+    }
+
+    public function filterEvents(DateTime $date) : Collection
+    {
+        return $this->events->filter(function(Event $event) use ($date) : bool {
+            return $event->isMatching($date);
         });
+    }
+
+    public function getOccurrences(DateTime $start, DateTime $end) : Collection
+    {
+        $period = new DatePeriod($start, new DateInterval('P1D'), $end);
+
+        $result = new ArrayCollection();
+
+        foreach($period as $day) {
+            foreach($this->events as $event) {
+                if($event->isMatching($day)) {
+                    $result->add(new Occurrence($day, $event));
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function addEvent(Event $event) : void
+    {
+        $this->events->add($event);
     }
 }
--- tests/Unit/CalendarTest.php
+++ tests/Unit/CalendarTest.php
@@ -4,11 +4,15 @@ namespace Test\Unit;
 
 use Calendar\Calendar;
 use Calendar\Event;
+use Calendar\Expression\AllOperator;
 use Calendar\Expression\DayOfWeek;
 use Calendar\Expression\EveryDay;
+use Calendar\Expression\GreatherThan;
+use Calendar\Expression\LowerThan;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
 
 class CalendarTest extends TestCase
 {
@@ -21,12 +25,94 @@ class CalendarTest extends TestCase
 
         $collection = new ArrayCollection([$event1, $event2, $event3]);
 
-        $calendar = new Calendar($collection);
+        $calendar = new Calendar(Uuid::uuid4(), $collection);
 
         $result = $calendar->filterEvents(new DateTime("last monday"));
 
         $this->assertCount(2, $result);
         $this->assertEquals($event1, $result[0]);
         $this->assertEquals($event3, $result[2]);
+    }
+
+    public function testAddEvent()
+    {
+        $collection = new ArrayCollection([]);
+        $calendar = new Calendar(Uuid::uuid4(), $collection);
+        $calendar->addEvent(new Event(new EveryDay()));
+
+        $this->assertCount(1, $collection);
+    }
+
+    public function testGetOccurrences()
+    {
+        $event = new Event(DayOfWeek::monday());
+
+        $collection = new ArrayCollection([$event]);
+
+        $calendar = new Calendar(Uuid::uuid4(), $collection);
+
+        $result = $calendar->getOccurrences(new DateTime("01.06.2018"), new DateTime("30.06.2018"));
+
+        $this->assertCount(4, $result);
+        $this->assertEquals(new DateTime("04.06.2018"), $result[0]->date());
+        $this->assertEquals(new DateTime("11.06.2018"), $result[1]->date());
+        $this->assertEquals(new DateTime("18.06.2018"), $result[2]->date());
+        $this->assertEquals(new DateTime("25.06.2018"), $result[3]->date());
+    }
+
+    public function testGetOccurrencesEmptyResult()
+    {
+        $event = new Event(DayOfWeek::monday());
+
+        $collection = new ArrayCollection([$event]);
+
+        $calendar = new Calendar(Uuid::uuid4(), $collection);
+
+        $result = $calendar->getOccurrences(new DateTime("last tuesday"), new DateTime("last tuesday"));
+
+        $this->assertCount(0, $result);
+    }
+
+
+    public function testGetOccurrencesOnSmallerPeriod()
+    {
+        $event = new Event(new AllOperator(
+            DayOfWeek::monday(),
+            new GreatherThan(new DateTime("01.01.2018")),
+            new LowerThan(new DateTime("31.12.2018"))
+        ));
+
+        $collection = new ArrayCollection([$event]);
+
+        $calendar = new Calendar(Uuid::uuid4(), $collection);
+
+        $result = $calendar->getOccurrences(new DateTime("01.06.2018"), new DateTime("30.06.2018"));
+
+        $this->assertCount(4, $result);
+        $this->assertEquals(new DateTime("04.06.2018"), $result[0]->date());
+        $this->assertEquals(new DateTime("11.06.2018"), $result[1]->date());
+        $this->assertEquals(new DateTime("18.06.2018"), $result[2]->date());
+        $this->assertEquals(new DateTime("25.06.2018"), $result[3]->date());
+    }
+
+    public function testGetOccurrencesOnBiggerPeriod()
+    {
+        $event = new Event(new AllOperator(
+            DayOfWeek::monday(),
+            new GreatherThan(new DateTime("01.06.2018")),
+            new LowerThan(new DateTime("30.06.2018"))
+        ));
+
+        $collection = new ArrayCollection([$event]);
+
+        $calendar = new Calendar(Uuid::uuid4(), $collection);
+
+        $result = $calendar->getOccurrences(new DateTime("01.01.2018"), new DateTime("31.12.2018"));
+
+        $this->assertCount(4, $result);
+        $this->assertEquals(new DateTime("04.06.2018"), $result[0]->date());
+        $this->assertEquals(new DateTime("11.06.2018"), $result[1]->date());
+        $this->assertEquals(new DateTime("18.06.2018"), $result[2]->date());
+        $this->assertEquals(new DateTime("25.06.2018"), $result[3]->date());
     }
 }
